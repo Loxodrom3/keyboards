f313f293bcc2139a5d9a34424be7beb3987a0a7a
Jon Proctor <jon.proctor@phototopo.com>
2021-04-19 00:04:56 -0600

    Update picoMidi.py
    a few updates

--- picoMidi.py
+++ picoMidi.py
@@ -29,26 +29,30 @@ from adafruit_midi.note_off         import NoteOff
 neo_pin = board.GP28
 numPixels = 17
 ORDER = neopixel.GRB
-keyLED = neopixel.NeoPixel(neo_pin, numPixels, brightness=0.50, auto_write=False, pixel_order=ORDER)
+keyLED = neopixel.NeoPixel(neo_pin, numPixels, brightness=0.06, auto_write=False, pixel_order=ORDER)
 offset = 0
 
-for i in range(0, numPixels, 1):
-    keyLED[i] = (100, 0, 0)
-keyLED.show()
-time.sleep(0.333)
+# Cycle through Red Green Blue on Start Up, then set to start color
+for h in range(0, 2, 1):
+    for i in range(0, numPixels, 1):
+        keyLED[i] = (100, 0, 0)  # Red
+        keyLED.show()
+    time.sleep(0.3)
+    for i in range(0, numPixels, 1):
+        keyLED[i] = (0, 100, 0)  # Green
+        keyLED.show()
+    time.sleep(0.3)
+    for i in range(0, numPixels, 1):
+        keyLED[i] = (0, 0, 100)  # Blue
+        keyLED.show()
+    time.sleep(0.3)
 
 for i in range(0, numPixels, 1):
-    keyLED[i] = (0, 100, 0)
+    keyLED[i] = (0, 50, 35)  # nice start up color
 keyLED.show()
-time.sleep(0.333)
 
-for i in range(0, numPixels, 1):
-    keyLED[i] = (0, 0, 100)
-keyLED.show()
-time.sleep(0.333)
-
-
-note_pins = [
+#pins used in JP's pico PCB (not using GP14 for midi, so I took it out of the list)
+pins = [
     board.GP27,
     board.GP26,
     board.GP22,
@@ -65,16 +69,17 @@ note_pins = [
     board.GP12,
     board.GP11,
     board.GP10,
-    board.GP14
+    board.GP6
 ]
 
 
 switches = [0,   1,  2,  3,
             4,   5,  6,  7,
             8,   9, 10, 11,
-            12, 13, 14, 15]
+            12, 13, 14, 15,
+            16]
 
-switch_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+switch_state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 clrRed = (200, 0, 0)
 clrGrn = (0, 50, 0)
@@ -82,42 +87,21 @@ clrPur = (100, 0, 100)
 clrBkg = (33, 0, 200)
 
 # ---------------------------
-# i2c setup, higher frequency for display refresh
-# i2c = busio.I2C(board.GP1, board.GP0, frequency=1000000)
-#  i2c display setup
-# display_bus = displayio.I2CDisplay(i2c, device_address=0x3D)
-#  i2c AW9523 GPIO expander setup
-# aw = adafruit_aw9523.AW9523(i2c)
 #  MIDI setup as MIDI out device
 midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
 
-#  button pins, all pins in order skipping GP15
-note_pins = [
-    board.GP27,
-    board.GP26,
-    board.GP22,
-    board.GP21,
-    board.GP20,
-    board.GP19,
-    board.GP18,
-    board.GP17,
-    board.GP16,
-    board.GP7,
-    board.GP8,
-    board.GP9,
-    board.GP13,
-    board.GP12,
-    board.GP11,
-    board.GP10
-]
- 
 note_buttons = []
- 
-for pin in note_pins:
+
+for pin in pins:
     note_pin = digitalio.DigitalInOut(pin)
     note_pin.direction = digitalio.Direction.INPUT
     note_pin.pull = digitalio.Pull.UP
     note_buttons.append(note_pin)
+
+# note_pin = digitalio.DigitalInOut(boardG.P16)
+# note_pin.direction = digitalio.Direction.INPUT
+# note_pin.pull = digitalio.Pull.DOWN
+# note_buttons.append(note_pin)
 
 #  note states
 note0_pressed = False
@@ -136,21 +120,21 @@ note12_pressed = False
 note13_pressed = False
 note14_pressed = False
 note15_pressed = False
+note16_pressed = False
 
 #  array of note states
 note_states = [note0_pressed, note1_pressed, note2_pressed, note3_pressed,
                note4_pressed, note5_pressed, note6_pressed, note7_pressed,
                note8_pressed, note9_pressed, note10_pressed, note11_pressed,
-               note12_pressed, note13_pressed, note14_pressed, note15_pressed]
+               note12_pressed, note13_pressed, note14_pressed, note15_pressed,
+               note16_pressed]
 
 #  array of default MIDI notes
-midi_notes = [60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75]
-
-
+midi_notes = [41, 37, 38, 39, 40, 36, 42, 43, 68, 69, 70, 71, 72, 73, 74, 75, 40]
 
 while True:
     #  MIDI input
-    for i in range(16):
+    for i in range(len(midi_notes)):
         buttons = note_buttons[i]
         #  if button is pressed...
         if not buttons.value and note_states[i] is False:
@@ -166,7 +150,3 @@ while True:
             note_states[i] = False
             keyLED[i] = (clrBkg)
             keyLED.show()
-
-
-
-
